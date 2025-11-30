af36c09344a8641171f84f5946fbc10567c084ae
Tommy0Storm <tommy@ferreira.mobi>
2025-11-30 21:03:57 +0200

    Update Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,8 @@
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  
-  return {
-    plugins: [react()],
-    base: './',
-    define: {
-      'import.meta.env.VITE_API_KEY': JSON.stringify(env.VITE_API_KEY || ''),
-      'import.meta.env.VITE_EMAILJS_SERVICE_ID': JSON.stringify(env.VITE_EMAILJS_SERVICE_ID || ''),
-      'import.meta.env.VITE_EMAILJS_TEMPLATE_ID': JSON.stringify(env.VITE_EMAILJS_TEMPLATE_ID || ''),
-      'import.meta.env.VITE_EMAILJS_PUBLIC_KEY': JSON.stringify(env.VITE_EMAILJS_PUBLIC_KEY || '')
-    }
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: './',
 });
