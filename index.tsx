982678c242a47a62a24b643177c0c45876aa0e48
Tommy0Storm <tommy@ferreira.mobi>
2025-12-01 17:19:49 +0200

    Fix passive event listener violations from third-party scripts

--- index.tsx
+++ index.tsx
@@ -3,6 +3,20 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+// PERFORMANCE: Force passive event listeners for scroll-blocking events
+// This fixes warnings from third-party scripts (Grammarly, Google, etc.)
+(function() {
+  const originalAddEventListener = EventTarget.prototype.addEventListener;
+  EventTarget.prototype.addEventListener = function(type, listener, options) {
+    const passiveEvents = ['touchstart', 'touchmove', 'touchend', 'wheel', 'mousewheel'];
+    if (passiveEvents.includes(type)) {
+      const opts = typeof options === 'object' ? { ...options, passive: true } : { passive: true, capture: !!options };
+      return originalAddEventListener.call(this, type, listener, opts);
+    }
+    return originalAddEventListener.call(this, type, listener, options);
+  };
+})();
+
 const rootElement = document.getElementById('root');
 if (!rootElement) {
   throw new Error("Could not find root element to mount to");
